951c8cdf4615629d6692505a08c8ebc1db37e8ae
bubul.com <amindalshad42@gmail.com>
2026-04-07 11:35:16 +0200

    commit2

--- js/_menu.js
+++ js/_menu.js
@@ -1,6 +1,4 @@
 createMenu('Statistics Template JS', [
-  { name: 'Välkommen', script: 'welcome.js' },
-  { name: 'Visa ett år', script: 'one-year.js' },
-  { name: 'Jämför två år', script: 'compare-two-years.js' },
-  { name: 'Hitta trender', script: 'trends.js' }
+  { name: 'Kom igång!', script: 'startpage.js' },
+  { name: 'Övningsinstruktioner', script: 'instructions.js' }
 ]);
