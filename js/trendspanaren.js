d8665290b860f01f71be9c4e20da134ca7dd660b
zizam-haha <zhisam.huang145@gmail.com>
2026-04-21 11:27:55 +0200

    Csv filen klart

--- js/trendspanaren.js
+++
@@ -1,8 +0,0 @@
-addMdToPage(`
-# Trendspaneren!!!!!
-  `);
-dbQuery.use('befolkning-sqlite');
-let income = await dbQuery.collection().find({}).limit(100);
-tableFromData({ data: income });
-console.log('income from mongodb', income);
-
