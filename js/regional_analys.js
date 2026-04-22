119e86c997ea58cd2e8b914da3c8da3fe99891c6
moawester <moa.wester@hotmail.se>
2026-04-22 16:57:00 +0200

    ändrat så kopplingarna fungerar, min sida

--- js/regional_analys.js
+++ js/regional_analys.js
@@ -4,60 +4,31 @@ addMdToPage(`
 Här undersöker vi hur olika län har röstat i riksdagsvalen 2018 och 2022. 
 `);
 
-async function runRegionalAnalysis() {
-  try {
-    // 1. Hämta länsinfo från SQLite
-    dbQuery.use('counties-sqlite');
-    let countyInfo = await dbQuery('SELECT * FROM counties');
-  
-
-    // 2. Hämta valdata 2022 från Neo4j
-    dbQuery.use('riksdagsval-neo4j');
-    let votes2022 = await dbQuery(`
-            MATCH (c:County)-[:HAS_MUNICIPALITY]->(m:Municipality)-[:HAS_VOTES]->(v:Votes {year: 2022})
-            RETURN c.name AS county, 
-                   m.name AS municipality,
-                   v.S AS S,
-                   v.M AS M,
-                   v.SD AS SD,
-                   v.V AS V,
-                   v.MP AS MP,
-                   v.C AS C,
-                   v.L AS L,
-                   v.KD AS KD
-        `);
-    console.log('Valdata 2022 hämtad:', votes2022.length, 'rader');
-
-    // 3. Hämta valdata 2018 från Neo4j
-    let votes2018 = await dbQuery(`
-            MATCH (c:County)-[:HAS_MUNICIPALITY]->(m:Municipality)-[:HAS_VOTES]->(v:Votes {year: 2018})
-            RETURN c.name AS county, 
-                   m.name AS municipality,
-                   v.S AS S,
-                   v.M AS M,
-                   v.SD AS SD,
-                   v.V AS V,
-                   v.MP AS MP,
-                   v.C AS C,
-                   v.L AS L,
-                   v.KD AS KD
-        `);
-    console.log('Valdata 2018 hämtad:', votes2018.length, 'rader');
-  } catch (error) {
-    console.error('Fel vid hämtning av data:', error);
-  }
-}
-
-console.log('Länsinfo hämtad:', countyInfo.length, 'län');
+dbQuery.use('counties-sqlite');
+let countyInfo = await dbQuery('SELECT * FROM countyInfo');
+console.log('Counties from SQLite', countyInfo);
 
 dbQuery.use('geo-mysql');
 let geoData = await dbQuery('SELECT * FROM geoData LIMIT 25');
 console.log('geoData from mysql', geoData);
 
 dbQuery.use('kommun-info-mongodb');
-// Prova olika samlingsnamn
 let kommunInfo = await dbQuery.collection('kommunInfo').find({}).limit(5);
 console.log('kommunInfo from MongoDB', kommunInfo);
 
-// Visa tabellen med geoData som fungerar
+dbQuery.use('riksdagsval-neo4j');
+let voteData = await dbQuery('MATCH (p:Partiresultat) RETURN p.kommun, p.parti, p.roster LIMIT 15');
+console.log('voteData from Neo4j', voteData);
+
+dbQuery.use('tatorter.db');
+let tatortData = await dbQuery('SELECT * FROM municipality_statistics');
+console.log('Tatorter from SQLite', tatortData);
+
+
+tableFromData({ data: countyInfo });
+
 tableFromData({ data: geoData });
+
+tableFromData({ data: voteData });
+
+tableFromData({ data: tatortData });
